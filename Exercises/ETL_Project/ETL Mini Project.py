759a8884055af77023ce9c3adecd688b0c89b79d
Matthew <m.emerson2001@outlook.com>
2025-01-27 20:25:23 +0000

    Updated ETL Mini Project

--- Exercises/ETL_Project/ETL Mini Project.py
+++ Exercises/ETL_Project/ETL Mini Project.py
@@ -1,33 +1,121 @@
 import csv # Import csv to read and write csv files
 
-transformed_data = [] # Create an empty list to write the transformed data to
+def extract_data(input_file): # Create extract function
+    transformed_data = [] # Create an empty list to write the transformed data to
+    try:
+        with open(input_file, mode='r') as reader:
+            csv_reader = csv.DictReader(reader)  # Use DictReader to read in rows as dictionaries
 
-with open('student_test_scores_extended(in).csv', mode='r') as reader:
-    csv_reader = csv.DictReader(reader) # Use DictReader to read in rows as dictionaries
+            columns = csv_reader.fieldnames
 
-    columns = csv_reader.fieldnames
+            columns_to_keep = [col for col in columns if
+                               "Score" in col or col == "Name"]  # Create list of fieldnames we want to keep based on criteria given
 
-    columns_to_keep = [col for col in columns if "Score" in col or col == "Name"] # Create list of fieldnames we want to keep based on criteria given
+            for row in csv_reader:  # Loop through each row
 
-    for row in csv_reader: # Loop through each row
+                filtered_data = {col: row[col] for col in
+                                 columns_to_keep}  # Creating new row dictionaries with only the columns we want
+                transformed_data.append((filtered_data, columns_to_keep))
 
-        filtered_data = {i: row[i] for i in columns_to_keep} # Creating new row dictionaries with only the columns we want
+    except FileNotFoundError:
+        print(f"Error: The file '{input_file}' was not found") # Error for when the file does not exist
+    except PermissionError:
+        print(f"Error: Do not have permission to read file '{input_file}'") # Error in the case of not having read permissions
+    except Exception as e:
+        print(f"Error: An unexpected error occurred: {e}") # General exception case
 
-        score_columns = [row[col] for col in columns_to_keep if "Score" in col] # Creates a list of score columns that will be used to compute the average score
+    return transformed_data, columns_to_keep
 
-        if score_columns:
-            average_score = sum(int(score) for score in score_columns) / len(score_columns) # if the column in the data is a "score column" it will compute the average
+def transform(extracted_data):
+    transformed_data = []
 
-        filtered_data['Average Score'] = average_score # Add the average_score column to the data
+    for row, columns_to_keep in extracted_data:
 
-        transformed_data.append(filtered_data) # Append each row dictionary to the transformed data list
+        try:
+            score_columns = [row[col] for col in columns_to_keep if
+                             "Score" in col]  # Specifically pick out score columns for average calculation
 
-print(transformed_data)
+            if score_columns:
+                average_score = sum(int(col) for col in score_columns) / len(
+                    score_columns)  # Computes the average score over all score columns
 
-with open('transformed_data.csv', mode="w", newline='') as writer:
-    csv_writer = csv.DictWriter(writer, fieldnames=columns_to_keep + ['Average Score']) # Use DictWriter as each row is a dictionary
+            row['Average Score'] = average_score  # Add the average score column
+            transformed_data.append(row)
 
-    csv_writer.writeheader()
+        except ValueError:
+            print(f"Error: Invalid data in score columns")
+        except Exception as e:
+            print(f"Error: An unexpected error occurred: {e}")
 
-    csv_writer.writerows(transformed_data)
+
+    return transformed_data
+
+def load(output_file, transformed_data, columns_to_keep):
+    try:
+        with open(output_file, mode="w", newline='') as writer:
+            csv_writer = csv.DictWriter(writer, fieldnames=columns_to_keep + [
+                'Average Score'])  # Use DictWriter as each row is a dictionary
+
+            csv_writer.writeheader()
+            csv_writer.writerows(transformed_data)
+
+    except PermissionError:
+        print(f"Error: You don't have permission to write to this file '{output_file}'") # If trying to overwrite existing file, this will happen if it is open
+    except Exception as e:
+        print(f"Error: An unexpected error occurred: {e}")
+
+def main():
+    input_file = "student_test_scores_extended(in).csv"
+    output_file = "transformed_data.csv"
+
+    try:
+        extracted_data, columns_to_keep = extract_data(input_file)
+
+        # If extraction step fails due to no data
+        if not extracted_data:
+            print("Stopping process due to not data")
+            return
+
+        transformed_data = transform(extracted_data)
+
+        if load(output_file, transformed_data, columns_to_keep): # This step checks if the load process succeeds
+
+            print("Data extracted, transformed, and loaded to \"transformed_data.csv\"")
+
+        else: print("Failure in loading process") # Prints alternative message if the process fails
+
+
+    except Exception as e:
+        print(f"Error: An unexpected error has occurred: {e}")
+
+
+if __name__=="__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
